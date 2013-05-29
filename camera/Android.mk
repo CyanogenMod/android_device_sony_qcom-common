08a25e67771a37e3fd3a870e1353727ec4e89259
Chirayu Desai <cdesai@cyanogenmod.org>
2013-05-29 21:32:52 +0530

    camera wrapper: initial commit
    Change-Id: I27e83c3f7f760fde6df693382a1ebb1fb3b5762f

---
+++ camera/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils libcutils
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.qcom
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
