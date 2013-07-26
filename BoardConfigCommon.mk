2d70bdca5b0de32ef37daecd2b10777d143341c4
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-07-26 22:34:12 +0200

    qcom-common: remove ARCH_ARM_HAVE_TLS_REGISTER
    Change-Id: Id7067523a111f7820dd901a4a019157207caf954

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,7 +26,6 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Power HAL
 TARGET_PROVIDES_POWERHAL := true
