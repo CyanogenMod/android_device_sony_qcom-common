bf9958477dfaf66949016c065d7d93f7b0249115
Chirayu Desai <cdesai@cyanogenmod.org>
2013-08-04 20:32:23 +0530

    Use only one set of adreno drivers.
    * All that differs now is the supported OpenGL ES version,
      3xx supports 3.0 whereas 2xx supports 2.0
    
    Change-Id: I205965c490d7547faa27596225b04f259ace7c75

--- qcom-common.mk
+++ qcom-common.mk
@@ -76,11 +76,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
     wifi.supplicant_scan_interval=15
 
-ifneq ($(USE_ADRENO_42),true)
-# OpenGL ES 3.0
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196608
-
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/qcom-common/qcom-common-vendor.mk)
-endif
