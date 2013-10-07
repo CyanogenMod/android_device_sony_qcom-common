ee6d14d407375dbba6fdda50b6572629fd88d6e9
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-10-07 17:12:54 +0200

    qcom-common: initial common lights hal
    Sony implementation use a service (illumination_service) to
    control led notification and do ALS in HW
    
    This wrapper implement ALS in the HAL like AOSP and forward
    led notification to the Sony light HAL which comunicate with
    the illumination_service
    
    Change-Id: I87fa502cb9167e79afa5fae157f5f93507bed79c

---
+++ liblight/Android.mk
@@ -0,0 +1,17 @@
+ifeq ($(TARGET_PROVIDES_LIBLIGHT),true)
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    lights.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libutils libcutils
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := lights.qcom
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+endif
