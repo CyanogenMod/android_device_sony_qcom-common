1ecefe8a028e2c41b08117e5a305beb19bef73d5
Adam Farden <adam@farden.cz>
2014-09-08 01:08:55 +0200

    Let us build the common Sony bits on msm8226
    Change-Id: I5b52dacc44eef951417b912107dbedba457bfb81

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),sony)
-ifeq ($(call is-board-platform-in-list,msm8974 msm8960 msm8660),true)
+ifeq ($(call is-board-platform-in-list,msm8974 msm8960 msm8660 msm8226),true)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
 endif
