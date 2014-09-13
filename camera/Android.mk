f9e976f798498b8d1d66feb1c8c3b39457b00021
Androguide <admin@androguide.fr>
2014-09-13 16:04:25 +0530

    camera: add a workaround for slow camera startup
    * On certain devices, the first camera initialisation
      takes long, and fails sometimes.
    * Add an app that opens and cloeses the camera on boot,
      so that when the user opens a camera app, it has
      already been initialised and will work.
    
    Signed-off-by: Chirayu Desai <cdesai@cyanogenmod.org>
    Change-Id: I0623a0777ad83741c1c36355dffa8adf3a510df3

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 ifeq ($(BOARD_CAMERA_HAVE_ISO),true)
@@ -17,3 +18,19 @@ LOCAL_MODULE := camera.qcom
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+# It's either this, or pointing to workaround/ for src,res and the manifest
+# LOCAL_FULL_MANIFEST_FILE := $(LOCAL_PATH)/workaround/$(LOCAL_MANIFEST_FILE)
+# LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/workaround/res
+# LOCAL_SRC_FILES := $(call all-java-files-under, src/workaround)
+LOCAL_PATH := $(LOCAL_PATH)/workaround
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := CameraWorkaround
+
+include $(BUILD_PACKAGE)
