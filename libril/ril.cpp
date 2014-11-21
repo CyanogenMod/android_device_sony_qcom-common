b4f74393245014a91f2e0ce55762aaf38fc9408e
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-11-21 11:02:02 +0100

    ril: port forward cm10/11 fix
    Change-Id: I89e70ea686f88353980038ad4798a120eb353c6e

--- libril/ril.cpp
+++ libril/ril.cpp
@@ -277,9 +277,11 @@ static void dispatchNVWriteItem(Parcel &p, RequestInfo *pRI);
 static void dispatchUiccSubscripton(Parcel &p, RequestInfo *pRI);
 static void dispatchSimAuthentication(Parcel &p, RequestInfo *pRI);
 static void dispatchDataProfile(Parcel &p, RequestInfo *pRI);
+static void dispatchNetworkManual (Parcel& p, RequestInfo *pRI);
 static int responseInts(Parcel &p, void *response, size_t responselen);
 static int responseStrings(Parcel &p, void *response, size_t responselen);
 static int responseString(Parcel &p, void *response, size_t responselen);
+static int responseStringsDataRegistrationState(Parcel &p, void *response, size_t responselen);
 static int responseVoid(Parcel &p, void *response, size_t responselen);
 static int responseCallList(Parcel &p, void *response, size_t responselen);
 static int responseSMS(Parcel &p, void *response, size_t responselen);
@@ -611,6 +613,45 @@ dispatchStrings (Parcel &p, RequestInfo *pRI) {
 
     if (pStrings != NULL) {
         for (int i = 0 ; i < countStrings ; i++) {
+#ifdef MEMSET_FREED
+            memsetString (pStrings[i]);
+#endif
+            free(pStrings[i]);
+        }
+
+#ifdef MEMSET_FREED
+        memset(pStrings, 0, datalen);
+#endif
+    }
+
+    return;
+invalid:
+    invalidCommandBlock(pRI);
+    return;
+}
+
+/** Callee expects const char * */
+static void
+dispatchNetworkManual (Parcel& p, RequestInfo *pRI) {
+    status_t status;
+    size_t datalen;
+    char **pStrings;
+
+    datalen = sizeof(char *) * 2;
+
+    startRequest;
+    pStrings = (char **)alloca(datalen);
+
+    pStrings[0] = strdupReadString(p);
+    appendPrintBuf("%s%s,", printBuf, pStrings[0]);
+    pStrings[1] = strdup("NOCHANGE");
+    closeRequest;
+    printRequest(pRI->token, pRI->pCI->requestNumber);
+
+    s_callbacks.onRequest(pRI->pCI->requestNumber, pStrings, datalen, pRI);
+
+    if (pStrings != NULL) {
+        for (int i = 0 ; i < 2 ; i++) {
 #ifdef MEMSET_FREED
             memsetString (pStrings[i]);
 #endif
@@ -2119,6 +2160,37 @@ static int responseStrings(Parcel &p, void *response, size_t responselen) {
     return 0;
 }
 
+/*
+ * RIL_RADIO_TECHNOLOGY: 19 (QCOM HSPAP_DC) ==> 30 (CM DCHSPAP)
+ */
+static int responseStringsDataRegistrationState(Parcel &p, void *response, size_t responselen) {
+
+    if (response == NULL && responselen != 0) {
+        ALOGE("invalid response: NULL");
+        return RIL_ERRNO_INVALID_RESPONSE;
+    }
+    if (responselen % sizeof(char *) != 0) {
+        ALOGE("invalid response length %d expected multiple of %d\n",
+            (int)responselen, (int)sizeof(char *));
+        return RIL_ERRNO_INVALID_RESPONSE;
+    }
+
+    char **p_cur = (char **) response;
+
+    if (p_cur[3] != NULL) {
+#ifdef RIL_LEGACY_PAP
+        if (strncmp(p_cur[3], "18", 2) == 0) {
+            ALOGE("DATA_REGISTRATION_STATE: stock radioTechnology=18 (QCOM HSPAP_DC) -> CyanogenMod radioTechnology=30 (CM DCHSPAP)");
+#else
+        if (strncmp(p_cur[3], "19", 2) == 0) {
+            ALOGE("DATA_REGISTRATION_STATE: stock radioTechnology=19 (QCOM HSPAP_DC) -> CyanogenMod radioTechnology=30 (CM DCHSPAP)");
+#endif
+            strncpy(p_cur[3], "30", 2);
+        }
+    }
+
+    return responseStrings(p, response, responselen);
+}
 
 /**
  * NULL strings are accepted
