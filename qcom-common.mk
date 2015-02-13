7ae86e8434e5748dd0465957441582d05aeb066d
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-02-14 10:08:57 +1030

    qcom-common: add languages_full.mk to all sony devices
    Change-Id: I3dc7d863e5dfb5d298fdeb7b4b0efb883da0f43e

--- qcom-common.mk
+++ qcom-common.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 COMMON_PATH := device/sony/qcom-common
 
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
