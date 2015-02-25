778e09d578891e327c3b81e83822dc75c90748e0
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-02-25 16:33:07 +1030

    qcom-common: Cleanup BoardConfig
    * TARGET_CPU_SMP defaults to true in CM-12.0/Lollipop
    
    Change-Id: I7d613b1085d8cb25b5b12ac2defc3b4c1fb14aa3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,7 +21,6 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_SMP := true
 
 # Bionic
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
