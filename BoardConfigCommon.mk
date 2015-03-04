ed75e7c7e96f9ded097c74ae6bf5cd496842b8c1
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-03-04 12:17:11 +0100

    remove deprecated egl.cfg
    Change-Id: I770772c249fb83730e5eede5e4193249c5f688b2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,10 +37,6 @@ TARGET_USES_C2D_COMPOSITION := true
 # Camera
 TARGET_PROVIDES_CAMERA_HAL := true
 
-ifeq ($(BOARD_EGL_CFG),)
-BOARD_EGL_CFG := device/sony/qcom-common/rootdir/system/lib/egl/egl.cfg
-endif
-
 # RIL
 BOARD_RIL_CLASS := ../../../device/sony/qcom-common/ril/
 
